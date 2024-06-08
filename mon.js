2aa787a02a271706fc6c4e7a8e77407783a030b8
ehroy <68073896+ehroy@users.noreply.github.com>
2024-06-08 09:48:19 +0700

    Update mon.js

--- mon.js
+++ mon.js
@@ -44,28 +44,24 @@ function tranferfunds(privateKey) {
         type: "function",
       },
       {
-        constant: false,
-        inputs: [
-          {
-            name: "_to",
-            type: "address",
-          },
-          {
-            name: "_value",
-            type: "uint256",
-          },
-        ],
-        name: "transfer",
-        outputs: [
-          {
-            name: "",
-            type: "bool",
-          },
-        ],
-        payable: false,
-        stateMutability: "nonpayable",
-        type: "function",
-      },
+            inputs: [{
+                internalType: "address",
+                name: "from",
+                type: "address"
+            }, {
+                internalType: "address",
+                name: "to",
+                type: "address"
+            }, {
+                internalType: "uint256",
+                name: "tokenId",
+                type: "uint256"
+            }],
+            name: "transferFrom",
+            outputs: [],
+            stateMutability: "nonpayable",
+            type: "function"
+        },
     ],
     new Wallet(privateKey, provider)
   );
